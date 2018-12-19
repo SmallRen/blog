434978419de8f61383759175d9036869523b081d
1406423298@qq.com <1406423298@qq.com>
2018-12-19 17:18:40 +0800

    提交

--- src/main/resources/static/js/footer.js
+++ src/main/resources/static/js/footer.js
@@ -1,8 +1,25 @@
+//初始化登录模块
 layui.config({
     base: '/static/js/modules/' //你存放新模块的目录，注意，不是layui的模块目录
 }).use('login'); //加载入口
 
+//初始化菜单
+layui.config({
+    base: '/static/js/util/'
+}).use(['menu'],function(){
+   menu = layui.menu;
+    menu.init();
+})
 
+layui.use('element', function(){
+    var element = layui.element; //导航的hover效果、二级菜单等功能，需要依赖element模块
+
+    //监听导航点击
+    element.on('nav', function(elem){
+        //console.log(elem)
+        layer.msg(elem.text());
+    });
+});
 var tab;
 function qqLoginImg() {
     var width = $(window).width();
